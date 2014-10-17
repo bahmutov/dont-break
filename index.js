c11bf5b686b879e5f37c1fb8037e19e6cdae1db4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-10-17 18:14:32 -0400

    working on testing

--- index.js
+++ index.js
@@ -2,18 +2,40 @@
 
 require('lazy-ass');
 var check = require('check-more-types');
+var q = require('q');
 
 function getDependents() {
-  var q = require('q');
   var read = require('fs').readFile;
   return q.nfcall(read, './.dont-break', 'utf-8')
+    .then(function (text) {
+      return text.split('\n').filter(function (line) {
+        return line.length;
+      });
+    })
     .fail(function () {
       // the file does not exist probably
       return [];
     });
 }
 
+function testDependent(dependent) {
+  la(check.unemptyString(dependent), 'invalid dependent', dependent);
+  console.log('testing', dependent);
+  return q(true);
+}
+
+function testDependents(dependents) {
+  la(check.array(dependents), dependents);
+  var testers = dependents.map(testDependent);
+  return testers.reduce(q.when, q(true));
+}
+
 getDependents().then(function (dependents) {
   la(check.arrayOfStrings(dependents), 'invalid dependents', dependents);
   console.log('dependents', dependents);
+
+  return testDependents(dependents)
+    .then(function () {
+      console.log('all dependents tested');
+    });
 }).done();
