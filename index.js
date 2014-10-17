504314574742563692355d725c619d492ef1ddea
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-10-17 18:01:31 -0400

    started code

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 
 require('lazy-ass');
 var check = require('check-more-types');
+
+function getDependents() {
+  var q = require('q');
+  var read = require('fs').readFile;
+  return q.nfcall(read, './.dont-break', 'utf-8')
+    .fail(function () {
+      // the file does not exist probably
+      return [];
+    });
+}
+
+getDependents().then(function (dependents) {
+  la(check.arrayOfStrings(dependents), 'invalid dependents', dependents);
+  console.log('dependents', dependents);
+}).done();
