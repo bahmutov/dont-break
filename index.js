8b2386a8bbd6a0506604077e1b489e2cfea71c8d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-10-19 16:09:12 -0400

    installing into tmp folder

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 require('lazy-ass');
 var check = require('check-more-types');
 var q = require('q');
+var install = require('npm-utils').install;
+la(check.fn(install), 'install should be a function', install);
+var path = require('path');
+var fs = require('fs');
+
+var pkg = require('./package.json');
+la(check.unemptyString(pkg.version), 'could not get package version', pkg);
 
 function getDependents() {
   var read = require('fs').readFile;
@@ -21,7 +28,20 @@ function getDependents() {
 function testDependent(dependent) {
   la(check.unemptyString(dependent), 'invalid dependent', dependent);
   console.log('testing', dependent);
-  return q(true);
+
+  var toFolder = '/tmp/' + dependent + '-' + pkg.version;
+
+  return install({
+    name: dependent,
+    prefix: toFolder
+  }).then(function () {
+    console.log('installed into', toFolder);
+    return path.join(toFolder, 'lib/node_modules/' + dependent);
+  }).then(function (folder) {
+    la(check.unemptyString(folder), 'expected folder', folder);
+    la(fs.existsSync(folder), 'expected existing folder', folder);
+    return folder;
+  });
 }
 
 function testDependents(dependents) {
@@ -30,12 +50,21 @@ function testDependents(dependents) {
   return testers.reduce(q.when, q(true));
 }
 
-getDependents().then(function (dependents) {
-  la(check.arrayOfStrings(dependents), 'invalid dependents', dependents);
-  console.log('dependents', dependents);
+function dontBreak() {
+  return getDependents().then(function (dependents) {
+    la(check.arrayOfStrings(dependents), 'invalid dependents', dependents);
+    console.log('dependents', dependents);
 
-  return testDependents(dependents)
-    .then(function () {
-      console.log('all dependents tested');
-    });
+    return testDependents(dependents)
+      .then(function () {
+        console.log('all dependents tested');
+      });
+  }).done();
+}
+
+testDependent('dont-break-foo-user')
+.then(function (folder) {
+  la(check.unemptyString(folder), 'expected install folder', folder);
+  la(fs.existsSync(folder), 'cannot find install folder', folder);
+  console.log('installed into folder', folder);
 }).done();
