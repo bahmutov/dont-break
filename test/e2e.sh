6ffb3cca13b05e5ee1c3951d709a47cceb3663fe
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-10-31 23:09:30 -0400

    removed test step

---
+++ test/e2e.sh
@@ -0,0 +1,19 @@
+echo "Testing dont-break"
+
+npm link
+echo "Linked current dont-break to global"
+
+echo "Creating test folder"
+folder=/tmp/test-dont-break
+rm -rf $folder
+mkdir $folder
+cd $folder
+echo "Created test folder $folder"
+
+echo "Cloning boggle"
+git clone https://github.com/bahmutov/boggle.git
+cd boggle
+npm install
+npm test
+echo "Boggle is working"
+
